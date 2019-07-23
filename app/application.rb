3f6643483faa9e68c937518936028d43fe7017a5
Trish Ogen <patriciaogen@gmail.com>
2019-07-23 03:08:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,12 @@
 class Application
 
-  def call
+  def call(env)
     resp = Rack::Response.new
 
     time = DateTime.now.strftime("%k:%M")
     noon = DateTime.now.change({hour: 12})
 
-    if time >= noon
+    if time > noon
       resp.write "Good Afternoon!"
     else
       resp.write "Good Morning!"
